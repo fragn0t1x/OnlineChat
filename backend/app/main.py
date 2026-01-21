96ffb3db4568760e8d3e0057b77dbb0a5f4750b2
andrejcernysov <fragn0t1x21@gmail.com>
2026-01-21 06:47:56 +0300

    update cors env

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,13 +38,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = os.getenv("CORS_ORIGINS", "").split(",")
-if origins == [""]:
-    origins = []
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # ← явно укажите ваш Tilda-сайт
+    allow_origins=["https://project5666571.tilda.ws"],  # ← явно укажите ваш Tilda-сайт
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
